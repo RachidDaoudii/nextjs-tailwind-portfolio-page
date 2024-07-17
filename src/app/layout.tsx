8f1ffed245a9f3d982b1bae71bf98868ad226618
RachidDaoudii <rachiddaoudi533@gmail.com>
2024-07-17 12:14:45 +0200

    modifier

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,7 @@ import "./globals.css";
 import type { Metadata } from "next";
 import { Roboto } from "next/font/google";
 import { Layout, FixedPlugin } from "@/components";
-
+import { SpeedInsights } from '@vercel/speed-insights/next';
 // const roboto = Roboto({
 //   subsets: ["latin"],
 //   weight: ["300", "400", "500", "700", "900"],
@@ -14,6 +14,7 @@ export const metadata: Metadata = {
   title: "Rachid Daoudi - Portfolio",
   description:
     "Rachid Daoudi is a Full-Stack Developer based in Morocco. Here, you'll get a glimpse of his journey in the world of web development, where creativity meets functionality.",
+
 };
 
 export default function RootLayout({
@@ -25,7 +26,10 @@ export default function RootLayout({
     <html lang="en">
       <head>
       <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css" integrity="sha512-SnH5WK+bZxgPHs44uWIX+LLJAJ9/2PkPKZ5QiAj6Ta86w+fsb2TkcmfRyVX3pBnMFcV7oQPJkl9QevSCWr3W6A==" crossOrigin="anonymous" referrerPolicy="no-referrer" />
-
+      <meta property="og:title" content="Rachid Daoudi - Portfolio" />
+      <meta property="og:description" content="Rachid Daoudi is a Full-Stack Developer based in Morocco. Here, you'll get a glimpse of his journey in the world of web development, where creativity meets functionality." />
+      <meta property="og:url" content="https://rachiddaoudi.vercel.app/" />
+      <meta property="og:image" content="https://ia.media-imdb.com/images/rock.jpg" />
         <script
           defer
           data-site="YOUR_DOMAIN_HERE"
@@ -37,6 +41,7 @@ export default function RootLayout({
         <Layout>
           {children}
           <FixedPlugin />
+          <SpeedInsights />
         </Layout>
       </body>
     </html>
